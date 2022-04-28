7e700d48f4e868afe7afc44c084851c29a85d9e8
Kévin Brèthes <bretheskevin@gmail.com>
2022-04-28 08:56:58 +0200

    feat: retrieve tweets

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_100718) do
     t.string "banner_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
