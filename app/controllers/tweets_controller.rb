1bf66cbb6d650b5827558eb4c80b4e89e5ea9b12
Gabriel Cancel <60512228+gabrielcancel@users.noreply.github.com>
2022-04-28 09:46:56 +0200

    feat: :construction: Update search bar
    We can search by hashtag, tweet content and user_tag

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,9 +8,12 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all.order("created_at DESC")
 
     if params[:search] && params[:search].start_with?("#")
-      @tweets = HashTag.where("name LIKE ?", "%#{params[:search].delete("#")}%").all.tweets
+      @tweets = HashTag.where("name LIKE ?", "%#{params[:search].delete("#")}%").first.tweets.order("created_at DESC")
+
+    elsif params[:search] && params[:search].start_with?("@")
+      @tweets = User.where("name_tag LIKE ?", "%#{params[:search].delete("@")}%").first.tweets.order("created_at DESC")
     else
-      @tweets = @tweets.where("message LIKE ?", "%#{params[:search]}%")
+      @tweets = @tweets.where("message LIKE ?", "%#{params[:search]}%").order("created_at DESC")
     end
   end
 
