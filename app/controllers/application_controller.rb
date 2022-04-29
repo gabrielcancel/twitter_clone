c055409782d71bc5ce41fe03db66612f17ee8743
Kévin Brèthes <bretheskevin@gmail.com>
2022-04-30 01:49:19 +0200

    feat: post tweet using tweet btn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :name_tag, :desc, :location, :website, :birthdate, :avatar_url, :banner_url])
-    end    
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name name_tag desc location website birthdate avatar_url banner_url])
+  end
+
 end
